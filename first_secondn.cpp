0252d37ed7690df79de76a114c3dbe67a5f85764
fupn26 <43246998+fupn26@users.noreply.github.com>
2018-09-19 21:51:11 +0200

    Add files via upload

---
+++ first_secondn.cpp
@@ -0,0 +1,18 @@
+//int main()
+//{
+//cout<<"Please enter your first and second names\n";
+//string first;
+//string second;
+//cin>> first >> second;
+//cout<<"Hello,"<<first<<" "<<second<<'\n';
+//}
+#include "std_lib_facilities.h"
+int main()
+{
+cout<< "Please enter your first an second names!\n";
+string first;
+string second;
+cin>> first >> second;
+string name = first+" "+second;
+cout<< "Hello, "<<name<<"!\n";
+}
